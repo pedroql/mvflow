22972c3f41dc5bb02ad9b079a325334f09f6b6fc
Pedro L <github@pedroloureiro.net>
2020-07-18 18:17:33 +0100

    Create a new module for an Android sample app

---
+++ samples/android-app/src/main/java/net/pedroloureiro/mvflow/samples/android/MainActivity.kt
@@ -0,0 +1,12 @@
+package net.pedroloureiro.mvflow.samples.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+}
