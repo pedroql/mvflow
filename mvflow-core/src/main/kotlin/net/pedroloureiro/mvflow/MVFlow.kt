a1019faa26792a07fa348a645b8abfa920fdd297
Pedro Loureiro <1092466+pedroql@users.noreply.github.com>
2020-12-29 21:51:39 +0000

    Review @solcott's PR (#25)
    * Update Kotlin to 1.4.21 and Coroutines to 1.4.2
    
    Update other gradle plugins to latest versions except Dokka as it requires other updates to it's configuration
    Update gradle to 6.7.1
    
    * Replace externalEffectsChannel with SharedFlow
    
    * Add test for external effect observer starting too late
    
    * Downgrade gradle plugin version
    
    To remain compatible with IntelliJ
    
    * Apply PR feedback
    
    Co-authored-by: Scott Olcott <scottolcott@gmail.com>

--- mvflow-core/src/main/kotlin/net/pedroloureiro/mvflow/MVFlow.kt
+++ mvflow-core/src/main/kotlin/net/pedroloureiro/mvflow/MVFlow.kt
@@ -3,12 +3,11 @@
 package net.pedroloureiro.mvflow
 
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.channels.BroadcastChannel
-import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onCompletion
@@ -197,26 +196,27 @@ private class MVFlowImpl<State, Action, Mutation, Effect>(
     private val defaultLogger: Logger = {}
 ) : MVFlowWithEffects<State, Action, Effect> {
     private val state = MutableStateFlow(initialState)
-    private val externalEffectChannel = BroadcastChannel<Effect>(Channel.BUFFERED)
+    private val externalEffectFlow =
+        MutableSharedFlow<Effect>(extraBufferCapacity = 64)
     private val mutex = Mutex()
 
     private inner class LoggingEffectSender(private val logger: Logger) : EffectSender<Effect> {
         override suspend fun send(effect: Effect) {
-            logger.invoke("Sending external effect $effect")
-            externalEffectChannel.send(effect)
+            logger.invoke("Emitting external effect $effect")
+            externalEffectFlow.emit(effect)
         }
 
         override fun offer(effect: Effect): Boolean {
-            logger.invoke("Offering external effect $effect")
-            return externalEffectChannel.offer(effect).also { accepted ->
+            logger.invoke("Trying to emit external effect $effect")
+            return externalEffectFlow.tryEmit(effect).also { accepted ->
                 if (!accepted) {
-                    logger.invoke("Channel rejected previous effect!")
+                    logger.invoke("SharedFlow rejected previous effect!")
                 }
             }
         }
     }
 
-    override fun observeEffects() = externalEffectChannel.openSubscription().consumeAsFlow()
+    override fun observeEffects() = externalEffectFlow.asSharedFlow()
 
     override fun takeView(
         viewCoroutineScope: CoroutineScope,
--- mvflow-core/src/test/kotlin/net/pedroloureiro/mvflow/MVFlowWithEffectsTest.kt
+++ mvflow-core/src/test/kotlin/net/pedroloureiro/mvflow/MVFlowWithEffectsTest.kt
@@ -90,6 +90,23 @@ internal class MVFlowWithEffectsTest {
     }
 
     @Test
+    fun `effect observer joins late`() = runBlockingTest {
+        val template = defaultTemplate()
+
+        lateinit var actionJob1: Job
+        val observed1 = mutableListOf<Effect>()
+
+        runTestTemplate(template) {
+            actionJob1 = launch {
+                delay(151)
+                mvflow.observeEffects().toList(observed1)
+            }
+        }
+        actionJob1.cancel()
+        assertEquals(4, observed1.size)
+    }
+
+    @Test
     fun `one observer can unsubscribe without affecting the others`() = runBlockingTest {
         val template = defaultTemplate()
 
